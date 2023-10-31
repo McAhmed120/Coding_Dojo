456f9a908e61003075abc6a365af329843c80513
McAhmed120 <ahmedksibi67@gmail.com>
2023-10-31 17:03:52 +0100

    like

---
+++ WebFundamentals/Week 2/Day 1/Core/like/script.js
@@ -0,0 +1,19 @@
+var count1=0;
+var likes1=document.querySelector("#likes1");
+
+function addLikes1() {
+    count1++;
+    likes1.innerText = count1;
+}
+var count2=0
+var likes2=document.querySelector("#likes2");
+function addLikes2(){
+    count2++
+    likes2.innerText = count2;
+}
+var count3=0;
+var likes3=document.querySelector("#likes3");
+function addlikes3(){
+    count3++;
+    like3.innerText = count3;
+}
