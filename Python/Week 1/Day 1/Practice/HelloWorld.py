a94b2a979b338dfd9def68348b407a0eef125613
McAhmed120 <ahmedksibi67@gmail.com>
2023-11-13 14:19:45 +0100

    HelloWorld

---
+++ Python/Week 1/Day 1/Practice/HelloWorld.py
@@ -0,0 +1,18 @@
+# 1. TASK: print "Hello World"
+print( "hello world" )
+# 2. print "Hello Noelle!" with the name in a variable
+name = "Noelle"
+a="Hello"
+print( a )	# with a comma
+print( name )	# with a +
+# 3. print "Hello 42!" with the number in a variable
+name = 42
+a="hello"
+print( a )	# with a comma
+print(name )	# with a +	-- this one should give us an error!
+# 4. print "I love to eat sushi and pizza." with the foods in variables
+fave_food1 = "sushi"
+fave_food2 = "pizza"
+print( f'i love to eat {fave_food1}' ) # with .format()
+print( f'i love to eat {fave_food2}' ) # with an f string
+
