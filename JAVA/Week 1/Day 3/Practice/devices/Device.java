e490e1ec8c84b3c74e19f85c9542812ab8be27ec
McAhmed120 <ahmedksibi67@gmail.com>
2024-01-29 14:21:29 +0100

    devices

---
+++ JAVA/Week 1/Day 3/Practice/devices/Device.java
@@ -0,0 +1,26 @@
+
+public class Device {
+	private int battery;
+	
+	public Device() {
+		this.battery=100;
+	};
+	public Device(int battery) {
+		this.battery=battery;
+	};
+	
+	
+	public void displaybattery() {
+		System.out.println("The  battery level is : %"+this.battery+"\n");
+	}
+	
+
+	public int getBattery() {
+		return battery;
+	}
+
+	public void setBattery(int battery) {
+		this.battery = battery;
+	}
+	
+}
---
+++ JAVA/Week 1/Day 3/Practice/devices/TestDevice.java
@@ -0,0 +1,17 @@
+
+public class TestDevice {
+
+	public static void main(String[] args) {
+		
+		Phone phone= new Phone();
+		phone.displaybattery();
+		phone.makeCall();
+		phone.makeCall();
+		phone.makeCall();
+		phone.playingGame();
+		phone.playingGame();
+		phone.charging();
+		phone.displaybattery();
+	}
+
+}
