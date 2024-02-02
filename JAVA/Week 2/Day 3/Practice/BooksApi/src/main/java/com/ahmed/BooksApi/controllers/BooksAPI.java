6cfc9ff155490affc9f40cc6681a725f2f52884a
McAhmed120 <ahmedksibi67@gmail.com>
2024-02-02 13:28:39 +0100

    BooksApi

---
+++ JAVA/Week 2/Day 3/Practice/BooksApi/src/main/java/com/ahmed/BooksApi/controllers/BooksAPI.java
@@ -0,0 +1,45 @@
+package com.ahmed.BooksApi.controllers;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.*;
+import com.ahmed.BooksApi.models.Book;
+import com.ahmed.BooksApi.services.BookService;
+
+@RestController
+public class BooksAPI {
+	private final BookService bookService;
+	public BooksAPI(BookService bookService){
+        this.bookService = bookService;
+    }
+    @GetMapping("/api/books")
+    public List<Book> index() {
+        return bookService.allBooks();
+    }
+    
+    @PostMapping("/api/books")
+    public Book create(@RequestParam(value="title") String title, @RequestParam(value="description") String desc, @RequestParam(value="language") String lang, @RequestParam(value="pages") Integer numOfPages) {
+        Book book = new Book(title, desc, lang, numOfPages);
+        return bookService.createBook(book);
+    }
+    
+    @GetMapping("/api/books/{id}")
+    public Book show(@PathVariable("id") Long id) {
+        Book book = bookService.findBook(id);
+        return book;
+    }
+    @PutMapping("/api/books/{id}")
+    public Book update(
+    		@PathVariable("id") Long id, 
+    		@RequestParam(value="title") String title, 
+    		@RequestParam(value="description") String desc, 
+    		@RequestParam(value="language") String lang,
+    		@RequestParam(value="pages") Integer numOfPages) {
+        Book book = bookService.updateBook(id, title, desc, lang, numOfPages);
+        return book;
+    }
+    @DeleteMapping("api/books/{id}")
+    public void destroy(@PathVariable("id") Long id) {
+        bookService.deleteBook(id);
+    }
+}
